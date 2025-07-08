b46401c882e58295a00e255018a361e890313d84
Stefan Rijnhart <stefan@opener.amsterdam>
2025-07-08 17:33:50 +0200

    [FIX] sentry: event dropped if include_context key not present
    Fixes
    
    ```
      File "/home/odoo/.local/lib/python3.12/site-packages/sentry_sdk/client.py", line 595, in _prepare_event
        new_event = before_send(event, hint or {})
                    ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
      File "/home/odoo/src/user/oca-server-tools/sentry/hooks.py", line 66, in before_send
        if event.setdefault("tags", {})["include_context"]:
           ~~~~~~~~~~~~~~~~~~~~~~~~~~~~^^^^^^^^^^^^^^^^^^^
    KeyError: 'include_context'
    2025-07-08 14:45:45,907 14296 INFO quatra-18-0-development-21441249 sentry_sdk.errors: before send dropped event
    ```
    
    Error is only logged when running Sentry in debug mode, otherwise events are dropped silently.

--- sentry/hooks.py
+++ sentry/hooks.py
@@ -63,7 +63,7 @@ def before_send(event, hint):
         if logger_name and not logging.getLogger(logger_name).propagate:
             return None
 
-    if event.setdefault("tags", {})["include_context"]:
+    if event.setdefault("tags", {}).get("include_context"):
         cxtest = get_extra_context(odoo.http.request)
         info_request = ["tags", "user", "extra", "request"]
 
