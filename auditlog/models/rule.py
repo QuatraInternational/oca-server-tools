0f667b8683d0711d7ae91a8ec91410d2471569e3
Stefan Rijnhart <stefan@opener.amsterdam>
2025-09-18 12:49:34 +0200

    [REF] OD-718, auditlog: hook to determine fields to exclude

--- auditlog/models/rule.py
+++ auditlog/models/rule.py
@@ -301,7 +301,7 @@ class AuditlogRule(models.Model):
             # stored in the database only at the end of the transaction, but
             # their values exist in cache.
             new_values = {}
-            fields_list = rule_model.get_auditlog_fields(self)
+            fields_list = rule.get_auditlog_fields(self)
             for new_record in new_records.sudo():
                 new_values.setdefault(new_record.id, {})
                 for fname, field in new_record._fields.items():
@@ -515,6 +515,11 @@ class AuditlogRule(models.Model):
 
         return export_data
 
+    def _get_fields_to_exclude(self):
+        """Hook to determine what fields to exclude"""
+        self.ensure_one()
+        return self.fields_to_exclude_ids.mapped("name")
+
     def _logging_enabled_for_user(self, user):
         """Hook to determine if logging is enabled for the user"""
         self.ensure_one()
@@ -547,8 +552,7 @@ class AuditlogRule(models.Model):
         http_session_model = self.env["auditlog.http.session"]
         model_model = self.env[res_model]
         model_id = self.pool._auditlog_model_cache[res_model]
-        auditlog_rule = self.env["auditlog.rule"].search([("model_id", "=", model_id)])
-        fields_to_exclude = auditlog_rule.fields_to_exclude_ids.mapped("name")
+        fields_to_exclude = self._get_fields_to_exclude()
 
         vals = {
             "model_id": model_id,
