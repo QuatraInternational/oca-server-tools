7e20cd74db37c7732954304223f737cd26e47c4e
Stefan Rijnhart <stefan@opener.amsterdam>
2026-02-19 10:02:13 +0100

    [REF] auditlog: adapt to new Odoo test patch checker
    See https://github.com/odoo/odoo/pull/247151
    
    * Register with the patch checker to prevent exhaustive logging
    
    ```
    odoo odoo.tests.common: /__w/server-tools/server-tools/auditlog/models/rule.py:232:_patch_method setting res.groups.unlink to <function AuditlogRule._make_unlink.<locals>.unlink_full at 0x7fa51ea90f70>
    ```
    
    * Align model patching with how it's done in base_automation to fully remove
      patched methods. Fixes #3439
    * Dismantle own patch checker.
    * Ensure no patching takes place in setUpClass.

--- auditlog/tests/common.py
+++ auditlog/tests/common.py
@@ -1,33 +1,39 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo.tests.common import TransactionCase
+from odoo.tests.common import SETATTR_SOURCES
+
+from odoo.addons.base.tests.common import BaseCommon
 
 
-class AuditLogRuleCommon(TransactionCase):
+class AuditLogRuleCommon(BaseCommon):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
         cls.models = set()
+        # Register rule.py as a known path in odoo.tests.common for patching methods
+        cls.setattr_sources = SETATTR_SOURCES.get("_patch_method")
+        SETATTR_SOURCES["_patch_method"] = "/auditlog/models/rule.py"
 
     @classmethod
     def create_rule(cls, vals):
-        rule = cls.env["auditlog.rule"].with_context(tracking_disable=True).create(vals)
-        # Keep track of patched models
-        cls.models |= set(rule.model_id.mapped("model"))
-        return rule
+        # Deprecated, just call `create` in your test setup.
+        return cls.env["auditlog.rule"].create(vals)
 
-    @classmethod
-    def tearDownClass(cls):
-        for rule in cls.env["auditlog.rule"].search([]):
+    def tearDown(self):
+        # Unsubscribe all rules in tearDown to prevent Odoo's patch checker in
+        # tests/common.py from ringing the alarm.
+        for rule in self.env["auditlog.rule"].search([]):
             try:
                 rule.unsubscribe()
             except KeyError:  # pragma: no cover
                 continue  # Model not loaded yet
+        return super().tearDown()
 
-        # Assert no patched methods remain
-        for model in cls.models:
-            for method in ["create", "read", "write", "unlink"]:
-                assert not hasattr(
-                    getattr(cls.env[model], method), "origin"
-                ), f"{model} {method} still patched"
+    @classmethod
+    def tearDownClass(cls):
+        # Deregister from Odoo's patch checker
+        if cls.setattr_sources:
+            SETATTR_SOURCES["_patch_method"] = cls.setattr_sources
+        else:
+            del SETATTR_SOURCES["_patch_method"]
         super().tearDownClass()
